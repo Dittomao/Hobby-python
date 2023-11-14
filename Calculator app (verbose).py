bc8a624aab55214cca2eabb52b8c96ab9975d6e8
Shreyas neerbhavi <82019785+Dittomao@users.noreply.github.com>
2023-11-14 05:36:20 +0000

    commit

--- Calculator app (verbose).py
+++ Calculator app (verbose).py
@@ -2,15 +2,15 @@ a=float(input("Enter the first value"))
 b=float(input("Enter the second value"))
 print("Type 1,2,3,4 for the following operations:")
 print("Operation menu:\n1. Addition\n2. Substraction\n3. Multiplication\n4. Division")
-op=input("Select Operation:")
-if op=="1":
+op=int(input("Select Operation:"))
+if op==1:
     print(a+b)
-if op=="2":
+if op==2:
     print(a-b)
-if op=="3":
+if op==3:
     print(a*b)
-if op=="4":
+if op==4:
     print(a/b)
-else:
+if op>4:
     print("Operation doesn't exist BITCH!")
 print("Arigato🎎!")
